384145e21d3c70496e4d18ce89c35cd4bb40bf35
James Shen <jjss886@gmail.com>
2020-02-22 18:03:05 -0500

    initial set up for card deck variables

---
+++ Blackjack/src/utils/utilities.js
@@ -0,0 +1,5 @@
+const suits = ["Spades", "Hearts", "Diamonds", "Clubs"],
+  values = ["2", "3", "4", "5", "6", "7", "8", "9", "10", "J", "Q", "K", "A"],
+  deck = [];
+
+export const createDeck = () => {};
