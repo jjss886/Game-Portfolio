2dd93ea526ebe41278a21cdf369b885fb8db2b76
James Shen <jjss886@gmail.com>
2020-02-23 14:04:11 -0500

    deck creation and shuffle

--- Blackjack/src/utils/utilities.js
+++ Blackjack/src/utils/utilities.js
@@ -1,5 +1,26 @@
 const suits = ["Spades", "Hearts", "Diamonds", "Clubs"],
-  values = ["2", "3", "4", "5", "6", "7", "8", "9", "10", "J", "Q", "K", "A"],
-  deck = [];
+  values = ["2", "3", "4", "5", "6", "7", "8", "9", "10", "J", "Q", "K", "A"];
+let deck = [];
 
-export const createDeck = () => {};
+export const createDeck = () => {
+  deck = new Array();
+  for (const val of values) {
+    for (const suit of suits) {
+      let weight = parseInt(val);
+      if (val === "J" || val === "Q" || val === "K") weight = 10;
+      if (val === "A") weight = 11;
+      deck.push({ Value: val, Suit: suit, Weight: weight });
+    }
+  }
+};
+
+export const shuffleDeck = () => {
+  for (let i = 0; i < 1000; i++) {
+    const one = Math.floor(Math.random() * deck.length),
+      two = Math.floor(Math.random() * deck.length),
+      temp = deck[one];
+
+    deck[one] = deck[two];
+    deck[two] = temp;
+  }
+};
